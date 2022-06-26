10515fd64231c096c55ab6ba747aa8fe215d6928
Sepehr Saeidi Farzad <ssaeidifarzad@gmail.com>
2022-06-26 13:04:05 +0430

    Space character added to nodes insert function implemented

--- src/TrieTree.java
+++ src/TrieTree.java
@@ -1,7 +1,44 @@
 import java.util.Scanner;
 
 public class TrieTree {
+    private Node root;
 
+    public void insertWord(String word) {
+        if (root == null)
+            root = new Node(false);
+        Node iterator = root;
+        char[] chars = word.toCharArray();
+        if (validityCheck(chars)) {
+            for (char c : chars) {
+                if (iterator.getNode(c) == null)
+                    iterator.insertNode(c, false);
+                iterator = iterator.getNode(c);
+            }
+            iterator.setWord(true);
+        }
+    }
+
+    private boolean validityCheck(char[] chars) {
+        for (char c : chars) {
+            if (!((c >= 65 && c <= 90) || (c >= 97 && c <= 122) || (c == 32))) {
+                System.out.println("Invalid character");
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean searchWord(String word) throws NullPointerException {
+        if (root == null) throw new NullPointerException("Empty tree");
+        Node iterator = root;
+        char[] chars = word.toCharArray();
+        for (char c : chars) {
+            if (iterator.getNode(c) == null)
+                return false;
+            iterator = iterator.getNode(c);
+        }
+        return iterator.isWord();
+    }
 }
 
 class Node {
@@ -23,13 +60,20 @@ class Node {
 
     public void insertNode(char c, boolean isWord) {
         if (children == null)
-            children = new Node[26];
-        children[c % 26] = new Node(isWord);
+            children = new Node[27];
+        if (c >= 65 && c <= 90)
+            c += 32;
+        if (c == 32)
+            children[26] = new Node(isWord);
+        else
+            children[c % 26] = new Node(isWord);
     }
 
     public Node getNode(char c) {
         if (children == null)
             return null;
+        if (c == 32)
+            return children[26];
         return children[c % 26];
     }
 
