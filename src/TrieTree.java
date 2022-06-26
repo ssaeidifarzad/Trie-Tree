e50f89211bb2b2cb76a2510f54dccd25ed80e3e2
Sepehr Saeidi Farzad <ssaeidifarzad@gmail.com>
2022-06-26 12:15:07 +0430

    Trie node implemented

--- src/TrieTree.java
+++ src/TrieTree.java
@@ -1,2 +1,43 @@
+import java.util.Scanner;
+
 public class TrieTree {
+
+}
+
+class Node {
+
+    private Node[] children;
+    private boolean isWord;
+
+    public Node(boolean isWord) {
+        this.isWord = isWord;
+    }
+
+    public boolean isWord() {
+        return isWord;
+    }
+
+    public void setWord(boolean word) {
+        isWord = word;
+    }
+
+    public void insertNode(char c, boolean isWord) {
+        if (children == null)
+            children = new Node[26];
+        children[c % 26] = new Node(isWord);
+    }
+
+    public Node getNode(char c) {
+        if (children == null)
+            return null;
+        return children[c % 26];
+    }
+
+}
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        scanner.close();
+    }
 }
